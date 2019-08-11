987497b99b834d1a8c5f49c6861d77a6560034d9
Giovanni Ficarra <giovannificarra95@gmail.com>
2019-08-11 20:11:18 +0200

    bug fix version 2

--- src/multiproc_utils.py
+++ src/multiproc_utils.py
@@ -14,42 +14,44 @@ def user_business_features(iterable):
     print("tot:", tot)
 
     for index, row in df_in.iterrows():
-        uid = index
-        vals = []
+        if not row.isna().all():
+            uid = index
+            vals = []
 
-        for cuisine in _cuisines_unique:
-            cuisine_av = cuisine + "_av"
-            vals.append(row[cuisine_av])
+            for cuisine in _cuisines_unique:
+                cuisine_av = cuisine + "_av"
+                vals.append(row[cuisine_av])
 
-        for cuisine in _cuisines_unique:
-            cuisine_av_bin = cuisine + "_av_bin"
-            vals.append(row[cuisine_av_bin])
+            for cuisine in _cuisines_unique:
+                cuisine_av_bin = cuisine + "_av_bin"
+                vals.append(row[cuisine_av_bin])
 
-        for cuisine in _cuisines_unique:
-            cuisine_av_real = cuisine + "_av_real"
-            vals.append(row[cuisine_av_real])
+            for cuisine in _cuisines_unique:
+                cuisine_av_real = cuisine + "_av_real"
+                vals.append(row[cuisine_av_real])
 
+            cols = ['av_rat_chinese_cuisine', 'av_rat_japanese_cuisine', 'av_rat_mexican_cuisine', 'av_rat_italian_cuisine',
+                    'av_rat_others_cuisine', 'av_rat_american_cuisine', 'av_rat_korean_cuisine', 'av_rat_mediterranean_cuisine',
+                    'av_rat_thai_cuisine', 'av_rat_asianfusion_cuisine',
 
-        cols = ['av_rat_chinese_cuisine', 'av_rat_japanese_cuisine', 'av_rat_mexican_cuisine', 'av_rat_italian_cuisine', 
-                'av_rat_others_cuisine', 'av_rat_american_cuisine', 'av_rat_korean_cuisine', 'av_rat_mediterranean_cuisine',
-                'av_rat_thai_cuisine', 'av_rat_asianfusion_cuisine',
+                   'av_rat_chinese_cuisine_bin', 'av_rat_japanese_cuisine_bin', 'av_rat_mexican_cuisine_bin',
+                   'av_rat_italian_cuisine_bin', 'av_rat_others_cuisine_bin', 'av_rat_american_cuisine_bin',
+                   'av_rat_korean_cuisine_bin', 'av_rat_mediterranean_cuisine_bin', 'av_rat_thai_cuisine_bin',
+                   'av_rat_asianfusion_cuisine_bin',
 
-               'av_rat_chinese_cuisine_bin', 'av_rat_japanese_cuisine_bin', 'av_rat_mexican_cuisine_bin', 
-               'av_rat_italian_cuisine_bin', 'av_rat_others_cuisine_bin', 'av_rat_american_cuisine_bin', 
-               'av_rat_korean_cuisine_bin', 'av_rat_mediterranean_cuisine_bin', 'av_rat_thai_cuisine_bin', 
-               'av_rat_asianfusion_cuisine_bin',
+                   'av_rat_chinese_cuisine_real', 'av_rat_japanese_cuisine_real', 'av_rat_mexican_cuisine_real',
+                   'av_rat_italian_cuisine_real', 'av_rat_others_cuisine_real', 'av_rat_american_cuisine_real',
+                   'av_rat_korean_cuisine_real', 'av_rat_mediterranean_cuisine_real', 'av_rat_thai_cuisine_real',
+                   'av_rat_asianfusion_cuisine_real']
 
-               'av_rat_chinese_cuisine_real', 'av_rat_japanese_cuisine_real', 'av_rat_mexican_cuisine_real', 
-               'av_rat_italian_cuisine_real', 'av_rat_others_cuisine_real', 'av_rat_american_cuisine_real', 
-               'av_rat_korean_cuisine_real', 'av_rat_mediterranean_cuisine_real', 'av_rat_thai_cuisine_real', 
-               'av_rat_asianfusion_cuisine_real']
+            df_out.loc[uid, cols] = vals
 
-        df_out.loc[uid, cols] = vals
+            if (count % 1000) == 0:
+                percent = (count / tot) * 100
+                print("process {4}\t- row {0}/{1}\t- {2:.3f}%\t- {3}".format(count, tot, percent, _time.asctime(), _os.getppid()))
 
-        if (count % 1000) == 0:
-            percent = (count / tot) * 100
-            print("process {4}\t- row {0}/{1}\t- {2:.3f}%\t- {3}".format(count, tot, percent, _time.asctime(), _os.getppid()))
-
-        count += 1
+            count += 1
+        else:
+            continue
 
     df_out.to_pickle(df_out_name)
