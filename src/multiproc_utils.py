f5aa8c7c4f2767d46c872676aa97b2ad67b09ed4
Giovanni Ficarra <giovannificarra95@gmail.com>
2019-09-04 03:39:03 +0200

    complete correction of the collaborative feature

--- src/multiproc_utils.py
+++ src/multiproc_utils.py
@@ -1,7 +1,6 @@
 import pandas as _pd
 import time as _time
 import os as _os
-import sys as _sys
 import traceback as _traceback
 from sklearn.metrics.pairwise import cosine_similarity as _cosine_similarity
 
@@ -22,6 +21,8 @@ cols_real = ['av_rat_chinese_cuisine_real', 'av_rat_japanese_cuisine_real', 'av_
            'av_rat_italian_cuisine_real', 'av_rat_others_cuisine_real', 'av_rat_american_cuisine_real', 
            'av_rat_korean_cuisine_real', 'av_rat_mediterranean_cuisine_real', 'av_rat_thai_cuisine_real', 
            'av_rat_asianfusion_cuisine_real']
+
+rev_cols = ['user_id', 'bin_truth_score', 'real_truth_score']
 
 def sub_user_business_features(df_in, df_out):
     is_nan = True
@@ -88,54 +89,70 @@ def user_business_features(iterable):
         df_out.to_pickle(df_out_name)
 
 
-def sub_set_coll_scores(review_set, users, restaurants):
+def sub_set_coll_scores(review_set, review_hist, users, restaurants):
     count = 1
     tot = review_set.shape[0]
-    
+    rest_id = None
+
     for rid, row in review_set.iterrows():
-        rest_id = row['business_id']
         user_id = row['user_id']
-        
-        a_u_r = row['cuisine_av_hist']
+        curr_user = users.loc[user_id]
+        old_rest_id = rest_id
+        rest_id = row['business_id']
+
+        if old_rest_id != rest_id:
+            review_rest_new = review_set.loc[review_set.business_id == rest_id, rev_cols]
+            review_rest_old = review_hist.loc[review_hist.business_id == rest_id, rev_cols]
+            review_rest = _pd.concat([review_rest_new, review_rest_old]).set_index('user_id').drop(user_id)
+            user_rest = users.loc[users.index.isin(review_rest.index)]
+            assert review_rest.shape[0] == user_rest.shape[0], "different shapes"
+
+        a_u = row['cuisine_av_hist']
         a_r = restaurants.loc[rest_id, 'average_stars']
-        user_sim = _cosine_similarity(users.loc[user_id, cols_std].values.reshape(1,-1), users[cols_std])
+        a_u_r = review_rest['stars']
+        user_sim = _cosine_similarity(curr_user[cols_std].values.reshape(1, -1), user_rest[cols_std])
         user_sim = _pd.Series(data=user_sim[0], index=users.index)
-        numerator = (user_sim * (a_u_r - a_r)).sum() -  1 * (a_u_r - a_r)
-        denominator = user_sim.sum() -1
-        
-        a_u_r_bin = row['cuisine_av_hist_bin']
+        user_sim.where(user_sim > 0.5, 0, inplace=True)
+        numerator = (user_sim * (a_u_r - a_r)).sum()
+        denominator = user_sim.sum() - 1
+
+        a_u_bin = row['cuisine_av_hist_bin']
         a_r_bin = restaurants.loc[rest_id, 'average_stars_bin']
-        user_sim = _cosine_similarity(users.loc[user_id, cols_bin].values.reshape(1,-1), users[cols_bin])
+        a_u_r_bin = review_rest['stars'] * review_rest['bin_truth_score']
+        user_sim = _cosine_similarity(curr_user[cols_bin].values.reshape(1, -1), user_rest[cols_bin])
         user_sim = _pd.Series(data=user_sim[0], index=users.index)
-        numerator_bin = (user_sim * (a_u_r_bin - a_r_bin)).sum() -  1 * (a_u_r_bin - a_r_bin)
-        denominator_bin = user_sim.sum() -1
-        
-        a_u_r_real = row['cuisine_av_hist_real']
+        user_sim.where(user_sim > 0.5, 0, inplace=True)
+        numerator_bin = (user_sim * (a_u_r_bin - a_r_bin)).sum()
+        denominator_bin = user_sim.sum() - 1
+
+        a_u_real = row['cuisine_av_hist_real']
         a_r_real = restaurants.loc[rest_id, 'average_stars_real']
-        user_sim = _cosine_similarity(users.loc[user_id, cols_real].values.reshape(1,-1), users[cols_real])
+        a_u_r_real = review_rest['stars'] * review_rest['real_truth_score']
+        user_sim = _cosine_similarity(curr_user[cols_real].values.reshape(1, -1), user_rest[cols_real])
         user_sim = _pd.Series(data=user_sim[0], index=users.index)
-        numerator_real = (user_sim * (a_u_r_real - a_r_real)).sum() -  1 * (a_u_r_real - a_r_real)
-        denominator_real = user_sim.sum() -1
-        
+        user_sim.where(user_sim > 0.5, 0, inplace=True)
+        numerator_real = (user_sim * (a_u_r_real - a_r_real)).sum()
+        denominator_real = user_sim.sum() - 1
+
         out_cols = ['coll_score', 'coll_score_bin', 'coll_score_real']
-        vals = [numerator/denominator, numerator_bin/denominator_bin, numerator_real/denominator_real]
+        vals = [a_u + numerator / denominator, a_u_bin + numerator_bin / denominator_bin,
+                a_u_real + numerator_real / denominator_real]
         review_set.loc[rid, out_cols] = vals
-        
+
         if count % 1000 == 0:
-            percent = (count/tot)*100
+            percent = (count / tot) * 100
             print("process {4}\t- row {0}/{1}\t- {2:.3f}%\t- {3}"
                   .format(count, tot, percent, _time.asctime(), _os.getpid()))
-        
+
         count += 1
 
 
 def set_coll_scores(iterable):
-    review_set, review_split_name, users, restaurants = iterable
-    
+    review_set, review_split_name, review_hist, users, restaurants = iterable
+
     try:
-        sub_set_coll_scores(review_set, users, restaurants)
+        sub_set_coll_scores(review_set, review_hist, users, restaurants)
     except:
         print("Unexpected error:", _traceback.format_exc())
     finally:
         review_set.to_pickle(review_split_name)
-
